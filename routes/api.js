e6e9097e862ffe920595479532da6273a8da541d
ROBERT Antoine <arobert@silene-habitat.com>
2019-11-13 16:01:05 +0100

    Ajout API pour savoir si un utilisateur est Admin

--- routes/api.js
+++ routes/api.js
@@ -6,22 +6,22 @@ const News = require("../models/news");
 const NewsSilene = require("../models/newsSilene");
 const Contacts = require("../models/contacts");
 const Users = require("../models/users");
-var logger  = require('../utils/logger');
+var logger = require("../utils/logger");
 require("dotenv").load();
+const ActiveDirectory = require("activedirectory");
 
 const db = process.env.MONGO_DB;
 mongoose.Promise = global.Promise;
 
 mongoose.connect(db, function(err) {
-  if (err) {  
-    logger.error.error("Erreur de connection à la base " , err);
+  if (err) {
+    logger.error.error("Erreur de connection à la base ", err);
     // console.error("Erreur de connection à la base " + err);
   }
 });
 
-
 router.get("/", function(req, res) {
-  logger.error.error("Erreur de connection à la base " , err);
+  logger.error.error("Erreur de connection à la base ", err);
   res.send("api works");
 });
 /**
@@ -30,7 +30,7 @@ router.get("/", function(req, res) {
  *   Users:
  *     properties:
  *       _id:
- *         type: string       
+ *         type: string
  *       prefs:
  *         type: [string]
  */
@@ -57,7 +57,7 @@ router.get("/", function(req, res) {
  *         type: string
  */
 
- /**
+/**
  * @swagger
  * definitions:
  *   Contacts:
@@ -106,14 +106,18 @@ router.get("/", function(req, res) {
  *         description: news
  */
 router.get("/news", function(req, res) {
-  
   logger.logApiAccess("GET", req.headers, "/api/news");
 
   NewsSilene.find({})
     .sort("-date")
     .exec(function(err, news) {
       if (err) {
-        logger.logError("Erreur dans la récupération des news en base", "GET", req.headers, "/api/news");
+        logger.logError(
+          "Erreur dans la récupération des news en base",
+          "GET",
+          req.headers,
+          "/api/news"
+        );
       } else {
         res.json(news);
       }
@@ -139,7 +143,12 @@ router.get("/contacts", function(req, res) {
     .sort({ sn: 1 })
     .exec(function(err, contacts) {
       if (err) {
-        logger.logError("Erreur dans la récupération des contacts en base", "GET", req.headers, "/api/contacts");
+        logger.logError(
+          "Erreur dans la récupération des contacts en base",
+          "GET",
+          req.headers,
+          "/api/contacts"
+        );
       } else {
         res.json(contacts);
       }
@@ -161,25 +170,26 @@ router.get("/contacts", function(req, res) {
  */
 router.put("/users/:id", function(req, res) {
   logger.logApiAccess("PUT", req.headers, "/api/users");
-  Users.findByIdAndUpdate(req.params.id,    
+  Users.findByIdAndUpdate(
+    req.params.id,
     {
-			$set: {
-				prefs: req.body.prefs								
-			}
-		},
-		{
-			new: true
-		},
-		function (err, updatedUser) {
-			if (err) {
-				console.log("Erreur dans la mise à jour du user", err);
-      } else {   
-        updatedUser.tokens = undefined
+      $set: {
+        prefs: req.body.prefs
+      }
+    },
+    {
+      new: true
+    },
+    function(err, updatedUser) {
+      if (err) {
+        console.log("Erreur dans la mise à jour du user", err);
+      } else {
+        updatedUser.tokens = undefined;
         res.json(updatedUser);
-			}
-		});
-  
-})
+      }
+    }
+  );
+});
 
 /**
  * @swagger
@@ -196,17 +206,77 @@ router.put("/users/:id", function(req, res) {
  */
 router.get("/users/:id", function(req, res) {
   logger.logApiAccess("GET", req.headers, "/api/users");
-  Users.findById(req.params.id,   
-    
-		function (err, user) {
-			if (err) {
-				console.log("Erreur dans la récupération du user " + req.params.id, err);
-      } else {   
-        user.tokens = undefined
+  Users.findById(
+    req.params.id,
+
+    function(err, user) {
+      if (err) {
+        console.log(
+          "Erreur dans la récupération du user " + req.params.id,
+          err
+        );
+      } else {
+        user.tokens = undefined;
         res.json(user);
-			}
-		});
-  
-})
+      }
+    }
+  );
+});
+
+/**
+ * @swagger
+ * /api/users/admin:
+ *   get:
+ *     description: Permet de savoir si l'utilisateur passé en paramètre est admin ou non 
+ *     tags:
+ *      - Users
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       204:
+ *         description: L'utilisateur est administrateur
+ *       404:
+ *         description: L'utilisateur n'est pas administrateur
+ */
+router.get("/users/admin/:id", function(req, res) {
+  var ldapConfig = {
+    url: process.env.LDAP_URL,
+    baseDN: process.env.LDAP_BASE_DN,
+    username: process.env.LDAP_USERNAME,
+    password: process.env.LDAP_PASSWORD
+  };
+
+  var ad = new ActiveDirectory(ldapConfig);
+
+  let groupName = "_Informatique";
+
+  ad.getUsersForGroup(groupName, function(err, users) {
+    if (err) {
+      logger.logError("ERROR: " + JSON.stringify(err));
+      return res.sendStatus(401);
+    }
+
+    if (!users) {
+      logger.logError("Group: " + groupName + " not found.");
+      return res.sendStatus(401);
+    }
+
+    var isAdmin;
+    users.forEach(element => {
+      if (
+        element &&
+        element.sAMAccountName.trim().toLowerCase() === req.params.id
+      ) {
+        isAdmin = "isAdmin";
+      }
+    });
+
+    if (isAdmin === "isAdmin") {
+      return res.sendStatus(204);
+    } else {
+      return res.sendStatus(404);
+    }
+  });
+});
 
 module.exports = router;
